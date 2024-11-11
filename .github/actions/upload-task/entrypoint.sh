0b6e505f69e5fe63ed16f3ad0f85ccfa40733e12
Kamil Olszewski <kls@devskiller.com>
2024-11-11 22:22:35 +0100

    dev

---
+++ .github/actions/upload-task/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -euo pipefail
+
+if [[ ! -d "$INPUT_PATH" ]]; then
+    echo "::error::Provided path $INPUT_PATH is not a directory"
+    exit 1
+fi
+zip -r task.zip "${{inputs.path}}" -x ".git/*" ".github/*"
+
+python3 publish_task.py
+if [[ "$INPUT_PUBLISH" == "true" ]]; then
+    python3 publish_task.py
+fi
