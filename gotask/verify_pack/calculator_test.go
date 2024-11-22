47bdd44af2a43e3dd9407881149b7869398c2126
Kamil Olszewski <67277979+kamil-olszewski-devskiller@users.noreply.github.com>
2024-11-22 18:00:43 +0100

    a

--- gotask/verify_pack/calculator_test.go
+++ gotask/verify_pack/calculator_test.go
@@ -8,22 +8,22 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestAddition_Assessment(t *testing.T) {
+func VerifyTestAddition_Assessment(t *testing.T) {
 	total := calculator.Add(-1, -1)
 	assert.EqualValues(t, -2, total, "add result was incorrect, got: %v, expected: %v.", total, -2)
 }
 
-func TestSubtraction_Assessment(t *testing.T) {
+func VerifyTestSubtraction_Assessment(t *testing.T) {
 	total := calculator.Subtract(2, -1)
 	assert.EqualValues(t, 3, total, "subtraction result was incorrect, got: %v, expected: %v.", total, 3)
 }
 
-func TestMultiplicationByZero_Assessment(t *testing.T) {
+func VerifyTestMultiplicationByZero_Assessment(t *testing.T) {
 	total := calculator.Multiply(2, 0)
 	assert.EqualValues(t, 0, total, "multiplication by 0 result was incorrect, got: %v, expected: %v.", total, 0)
 }
 
-func TestDivisionByZero_Assessment(t *testing.T) {
+func VerifyTestDivisionByZero_Assessment(t *testing.T) {
 	total := calculator.Divide(4, 0.0)
 	assert.EqualValues(t, math.Inf(1), total, "division by 0 result was incorrect, got: %v, expected: %v.", total, math.Inf(1))
 }
